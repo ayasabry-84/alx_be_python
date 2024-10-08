8f4e567afaadb51d3515486486b6396fa903c92f
Aya Sabry <ayasabryy017@gmail.com>
2024-10-09 02:48:59 +0300

    first question is added

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance = 0 ):
+        self.account_balance = account_balance
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+        print(f"Deposited: {amount}. New balance: {self.account_balance}")
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount 
+            print(f"Withdrew: {amount}. New balance: {self.account_balance}")
+            return True
+        else:
+            print("Insufficient funds.")
+            return False
+
+    def display_balance(self):
+        print(f"Current balance: {self.account_balance}")
